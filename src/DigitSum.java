03e0284ddd91a822cea455545090011db40c561d
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2024-09-25 22:22:19 -0400

    Second Commit

--- src/DigitSum.java
+++ src/DigitSum.java
@@ -7,6 +7,7 @@ public class DigitSum {
         System.out.println(sumDigits(34));
         System.out.println(sumDigits(-54));
 
+
     }
 
     // Method that tests for negative numbers and returns the sum of each digit in an integer.
